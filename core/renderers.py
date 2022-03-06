d3a5e887d0c46613adc5dde7df2a3fea7384f680
Khemmatat Theanvanichpant <khemmatat.thean@gmail.com>
2022-03-06 22:53:20 +0700

    fix xlsx media type

--- core/renderers.py
+++ core/renderers.py
@@ -2,7 +2,7 @@ from drf_spectacular import renderers
 
 
 class XLSXRenderer(renderers.BaseRenderer):
-    media_type = "application/vnd.ms-excel"
+    media_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     format = "xlsx"
     charset = None
     render_style = "binary"
